4ad9d5f8002abd0f9d6db6ed1ffc6e9291ff7d98
ganesh <ganesh.chauhan@amniltech.com>
2017-11-04 18:53:00 +0545

    apli update

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -6,7 +6,8 @@ import { tokenNotExpired } from 'angular2-jwt';
 @Injectable()
 export class AuthService {
 
-  domain = "http://localhost:8081/"; // Development Domain - Not Needed in Production
+  // domain = "http://localhost:8081/"; // Development Domain - Not Needed in Production
+  domain = ""; 
   authToken;
   user;
   options;
